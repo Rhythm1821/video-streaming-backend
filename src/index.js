99d1f111ca0195c8907a4838eaffc1823d574d0e
Rhythm1821 <alamrawat1821@gmail.com>
2024-06-17 13:36:14 +0530

    index and app files updated

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
 import dotenv from "dotenv"
 import express from "express"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({ path: "./.env" })
 
-const app = express()
 
 connectDB()
 .then(()=>{
